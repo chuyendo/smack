614c4e246a792e6ca5ee1fe9da494e716a146cec
Cherry <hanhchuyen6791@yahoo.com>
2018-02-06 23:29:13 +0700

    lesson 20

--- Smack/Smack/Services/MessageService.swift
+++ Smack/Smack/Services/MessageService.swift
@@ -16,11 +16,12 @@ class MessageService {
     
     var channels = [Channel]()
     
-//    func findAllChannel(completion: @escaping CompletionHandler) {
-//        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
-//            if response.result.error == nil{
-//                guard let data = response.data else { return }
-//
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+
 //                do {
 //                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
 //                } catch let error {
@@ -28,23 +29,24 @@ class MessageService {
 //                }
 //                print(self.channels)
     
-//                if let json = try! JSON(data: data).array {
-//                    for item in json {
-//                        let name = item["name"].stringValue
-//                        let channelDescription = item["description"].stringValue
-//                        let id = item["_id"].stringValue
-//                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
-//                        self.channels.append(channel)
-//                    }
-//                    completion(true)
-//                }
+                if let json = try! JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels[0].channelTitle)
+                    completion(true)
+                }
                 
-//            } else {
-//                completion(false)
-//                debugPrint(response.result.error as Any)
-//            }
-//            
-//        }
-//    }
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+            
+        }
+    }
     
 }
